d8da85898097bad8070ef7074886c3de2c56de68
Rajat Bista <rajat.bista@gmail.com>
2020-07-20 21:00:52 +0545

    Updates

---
+++ precache-manifest.389e48c7a1aa12890760e1dfb9a3b11d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22932d4e495beb87f533f9144208bbe0",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "abfe9d1985d7fd7d02a6",
+    "url": "/monsters-rolodex/static/css/main.a616ca45.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abfe9d1985d7fd7d02a6",
+    "url": "/monsters-rolodex/static/js/main.b34584d4.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
